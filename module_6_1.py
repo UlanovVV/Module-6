2385505fd2fe3bf10ed25395904f9dfb185e5ec4
UlanovVV <vovancux39@gmail.com>
2024-07-08 21:06:54 +0200

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,52 +1,46 @@
 """ Создаем родительские классы животных и растений"""
+
 class Animal:
+    alive = True
+    fed = False
+
     def __init__(self, name):
         self.name = name
-        self.alive = True
-        self.fed = False
+
+    def eat(self, food):  #Логика для еды. Если растение съедобное то меняем статус на накормелен, иначе умираем
+        if food.edible:
+            self.fed = True
+            print(f"{self.name} съел {food.name}")
+        else:
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name} и погиб.")
 
 
 class Plant:
+    edible = False
+
     def __init__(self, name):
         self.name = name
-        self.edible = False
+
 
 """ Создаем классы наследников , где животные будут есть растения"""
 
+
 class Mammal(Animal):
-    def __init__(self, name):
-        super().__init__(name) #Позволяет использовать родительские классы
-       
-    def eat(self, food): #""" Логика для еды. Если растение съедобное то меняем статус на накормелен, иначе умираем"""
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
-        else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name} и погиб.")
+    pass
+
 
 class Predator(Animal):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
-        else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name} и погиб.")
+    pass
 
 
 class Flower(Plant):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
 
 
 class Fruit(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-        self.edible = True
+    edible = True
+
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
